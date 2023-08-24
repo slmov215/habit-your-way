4fe18b5e0951f3000585687dee20c34bd2c4ba0c
Stephen <127278005+slmov215@users.noreply.github.com>
2023-08-24 18:22:37 -0400

    fixed /graphql error, edit auth file and need to work on typedefs for uploadPhoto

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-// const cloudinary = require('./config/cloudinaryConfig');
-// const multer = require('multer');
+const cloudinary = require('./config/cloudinaryConfig');
+const multer = require('multer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,18 +1,14 @@
-const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
-    extensions: {
-      code: 'UNAUTHENTICATED',
-    },
-  }),
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+  // function for our authenticated routes
+  authMiddleware: function ({req}) {
+    // allows token to be sent via  req.query or headers
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -23,6 +19,7 @@ module.exports = {
       return req;
     }
 
+    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -30,10 +27,11 @@ module.exports = {
       console.log('Invalid token');
     }
 
+    // send to next endpoint
     return req;
   },
-  signToken: function ({ firstName, email, _id }) {
-    const payload = { firstName, email, _id };
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
