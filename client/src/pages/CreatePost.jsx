619cad79041e4150ca0fce29ca5c3c97c32c1e5e
Soleiles <ktaing789@gmail.com>
2023-08-24 20:06:01 -0400

    updated createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 
 export default function CreatePost({ handlePageChange }) {
     return (
-        <section>
-            <article className='header'>
-                <h1>Create A Post!</h1>
-            </article> 
-        </section>
+        <div className="form">
+            <h2>Create a Post!</h2>
+            <article className="post-form">
+                <Form>
+                    <Form.Group className='mb-3' controlId='formBasicText'>
+                        <Form.Label>Activity:</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder='Ex: Morning Jog'
+                        />
+                    </Form.Group>
+
+                    <Form.Group className='mb-3' controlId='formControlTextarea'>
+                        <Form.Label>Description:</Form.Label>
+                        <Form.Control
+                            as='textarea'
+                            placeholder="Ex: I ran 4 miles today!"
+                            rows='4'
+                        />
+                    </Form.Group>
+                    <Button variant='primary' type='submit'>Create Post</Button>
+                </Form>
+            </article>
+        </div>
     )
 }
