4f2011858faece5ec19c829d5bcfbe29248a6a94
Joseph Pham <joeypham13@gmail.com>
2023-08-31 21:19:43 -0400

    made the login look like the sign up

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
+import { Container, Row, Col } from 'react-bootstrap';
 
 import AuthService from "../utils/auth";
 
@@ -42,14 +43,15 @@ const Login = () => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
+    <Container>
+    <Row className="justify-content-center">
+      <Col md={6}>
+        <h2 className="text-center mt-5">Login</h2>
         <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
           <div className="card-body">
             {data ? (
               <p>
-                Success! You may now head{" "}
+                Success! You may now head{' '}
                 <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
@@ -72,7 +74,7 @@ const Login = () => {
                 />
                 <button
                   className="btn btn-block btn-primary"
-                  style={{ cursor: "pointer" }}
+                  style={{ cursor: 'pointer' }}
                   type="submit"
                 >
                   Submit
@@ -87,8 +89,9 @@ const Login = () => {
             )}
           </div>
         </div>
-      </div>
-    </main>
+      </Col>
+    </Row>
+  </Container>
   );
 };
 
